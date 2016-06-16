7d7bcc2f2c2ed7fe05abe5dd9d400024b336acb6
Matt Ma <bigmabig@gmail.com>
2016-06-16 11:32:19 -0700

    add ngrx/store and its ecosystem

--- src/system-config.ts
+++ src/system-config.ts
@@ -7,11 +7,22 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  '@ngrx': 'vendor/@ngrx',
+  'ngrx-store-logger': 'vendor/ngrx-store-logger'
 };
 
 /** User packages configuration. */
 const packages: any = {
+  'ngrx-store-logger': { main: 'dist/index' }
 };
+
+// loads up ngrx/store package
+const ngrxPkgs: string[] = [
+  'store',
+  'core',
+  'effects'
+];
+ngrxPkgs.forEach((pkg) => packages[`@ngrx/${pkg}`] = { main: 'index', format: 'cjs' });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
