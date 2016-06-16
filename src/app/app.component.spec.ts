34f2fd018a40cccc0a8abdd77decbc29f752e0dd
Matt Ma <bigmabig@gmail.com>
2016-06-16 09:57:24 -0700

    add README file, update to angular cli beta6 spes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
+/* tslint:disable:no-unused-variable */
+
 import {
+  beforeEach,
   beforeEachProviders,
   describe,
+  xdescribe,
   expect,
   it,
+  xit,
+  async,
   inject
 } from '@angular/core/testing';
 import { AppComponent } from './app.component';
